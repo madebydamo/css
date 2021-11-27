777e9ffc42ac6401fef73105f5a968437f008dc8
Klark007 <jan.grunder@yahoo.de>
2021-11-27 13:18:12 +0100

    Drawing walls

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from raylib import *
 import numpy as np
 
 import creature
+import wall
 
 class UI:
     def __init__(self, windowWidth, windowHeight, worldWidth, worldHeight):
@@ -41,7 +42,8 @@ class UI:
         DrawText(bytes(str(mouseX)+","+str(mouseY), 'utf-8'), 10, 10, 12, GRAY)
 
         EndDrawing()
-    def drawWindow(self, creatures):
+
+    def drawWindow(self, creatures, walls = []):
         BeginDrawing()
         ClearBackground(RAYWHITE)
 
@@ -55,7 +57,13 @@ class UI:
             normalizedVel = (creature.velocity / creature.maxVelocity) * 1.25
             (lineX, lineY) = self.worldToWindow(creature.location[0] + normalizedVel[0],
                                                 creature.location[1] + normalizedVel[1])
-            DrawLine(posX, posY, lineX, lineY, BLACK)
+            DrawLine(posX, posY, lineX, lineY, GRAY)
+
+        for wall in walls:
+            (startX, startY) = self.worldToWindow(wall.start[0], wall.start[1])
+            (endX, endY) = self.worldToWindow(wall.end[0], wall.end[1])
+
+            DrawLine(startX, startY, endX, endY, BLACK)
 
         # output current mouse position
         (mouseX, mouseY) = self.windowToWorld(GetMouseX(), GetMouseY())
@@ -69,8 +77,7 @@ class UI:
 
 
 def showSimulation(filepath):
-    import numpy
-    creatures = numpy.load(filepath)
+    creatures = np.load(filepath)
     ui = UI(800, 800, 40, 40)
 
     i = 0
@@ -86,23 +93,30 @@ def showSimulation(filepath):
     ui.closeWindow()
 
 def testUI():
-    locations = np.array([[1,1],[1,1.1],[3,1]])
-    goals = np.array([[1,9],[1,9.1],[3,9]])
+    #locations = np.array([[1,1],[1,1.1],[3,1]])
+    #goals = np.array([[1,9],[1,9.1],[3,9]])
+    locations = np.array([[2.2,1],[2,9]])
+    goals = np.array([[[2.2,11],[3,11]],[[2,1],[2,9],[2,1]]])
+    wall_positions = np.array([[2.5,0.5],[2.5,10]])
 
     """a = creature.Creature(locations[0],goals[0])
     b = creature.Creature(locations[1],goals[1])
     creatures = [a, b]"""
 
     creatures = []
-    for i in range(3):
+    for i in range(locations.ndim):
         creatures.append(creature.Creature(locations[i],goals[i]))
 
-    ui = UI(800, 500, 20, 10)
+    walls = []
+    for i in range(0, wall_positions.ndim, 2):
+        walls.append(wall.Wall(wall_positions[i], wall_positions[i+1]))
+
+    ui = UI(800, 500, 15, 15)
 
     while not WindowShouldClose():
-        ui.drawWindow(creatures)
+        ui.drawWindow(creatures, walls)
 
-        dt = GetFrameTime()
+        dt = 0.01 # GetFrameTime()
 
         for j,robot in enumerate(creatures):
             robot.update(simple.socialForce, creatures, dt)
