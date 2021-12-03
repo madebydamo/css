c16de17480f860fa412e6219ddcb905514b341ab
Klark007 <jan.grunder@yahoo.de>
2021-12-03 22:35:01 +0100

    Distributed training with saving of parameters and fitness

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import random
-from deap import creator, base, tools, algorithms
 import time
+import os
+
+from deap import creator, base, tools, algorithms
 from scoop import futures
+import numpy as np
 
 from models import simple as simulationcase
 import simulation
@@ -12,42 +15,61 @@ def evalData(params):
         return simulationcase.socialForceWithParams(creatureA, creatures, objects, dt, params)
     return [simulation.simulate(socialForce, None, 1.0/30, 10, False)]
 
-
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-toolbox.register("attr_param", random.uniform, 0.01, 2.)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_param, n=simulationcase.paramnr)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-toolbox.register("evaluate", evalData)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
 toolbox.register("map", futures.map)
 
-population = toolbox.population(n=50)
+if __name__ == '__main__':
+    toolbox.register("attr_param", random.uniform, 0.01, 2.)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_param, n=simulationcase.paramnr)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-NGEN = 50
-print("start training")
-dirName = f'./tmp/evol{time.time()}'
-for gen in range(NGEN):
-    offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
-    fits = toolbox.map(toolbox.evaluate, offspring)
+    toolbox.register("evaluate", evalData)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
 
-    for fit, ind in zip(fits, offspring):
-        ind.fitness.values = fit
+    population = toolbox.population(n=32)
 
-    print(f"gen {gen} trained")
-    population = toolbox.select(offspring, k=len(population))
-    print(tools.selBest(population, k=1))
-    print(simulation.simulateWithParams(
-        simulationcase.socialForceWithParams,
-        None,
-        1.0/30,
-        10,
-        tools.selBest(population, k=1)[0],
-        f'{dirName}/gen{gen}.npy'))
+    params = []
+    fitness = []
+
+    NGEN = 5
+    print("start training")
+    dirName = f'./tmp/evol{time.time()}'
+    for gen in range(NGEN):
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+
+        print(f"gen {gen} trained")
+        population = toolbox.select(offspring, k=len(population))
+
+        params.append(tools.selBest(population, k=1))
+        fitness.append(simulation.simulateWithParams(
+            simulationcase.socialForceWithParams,
+            None,
+            1.0/30,
+            10,
+            tools.selBest(population, k=1)[0],
+            f'{dirName}/gen{gen}.npy'))
+
+        print(tools.selBest(population, k=1))
+        print(simulation.simulateWithParams(
+            simulationcase.socialForceWithParams,
+            None,
+            1.0/30,
+            10,
+            tools.selBest(population, k=1)[0],
+            f'{dirName}/gen{gen}.npy'))
+
+
+    np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+
+    np.save( f'{dirName}/fitness.npy', np.array(fitness))
+    np.save(f'{dirName}/params.npy', np.array(params))
