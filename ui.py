bb9bc8c30e4a3b4e48f747a1b6a388f1c253be2a
Klark007 <jan.grunder@yahoo.de>
2021-12-04 12:06:06 +0100

    UI Comments

--- ui.py
+++ ui.py
@@ -6,16 +6,23 @@ import wall
 import simulation
 
 """
-    Class used to show a simulation on a window:
-        - additional functions to convert between window and world space
+    Class used to show a simulation on a window.
+    The simulation is either done at the same time (with possible slow down to rendering) or can be done in advanced 
+    and then be visualized using the method.
+    
+    The world is assumed to always be starting at (0,0) and only expand in the positive x and y direction. Creatures and objects may be placed outside those bounds
+    and still work as expected but won't be shown on screen.
 """
 
 
 class UI:
+    # initilazes the window and sets the size of both the window and the simualted world
     def __init__(self, windowWidth, windowHeight, worldWidth, worldHeight):
         InitWindow(windowWidth, windowHeight, b"Social Forces Model")
         SetTargetFPS(60)
 
+        # important: creatures may appear to be walking much faster into 1 axis than in the other one.
+        # This may happen if the ration of world and screen don't match up
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
 
@@ -23,14 +30,14 @@ class UI:
         self.worldHeight = worldHeight
         self.creatureRadius = (windowWidth / worldWidth) / 4
 
+    # converts from world to window space
     def worldToWindow(self, x, y): # inverted coordinate y space
-        # print(f"x: {x}, y: {y}")
         return int(x * self.windowWidth / self.worldWidth), int(self.windowHeight - y * self.windowHeight / self.worldHeight)
 
+    # converts from window to world space (useful for finding out where cursor is in the world)
     def windowToWorld(self, x, y):
         return float(x * self.worldWidth / self.windowWidth), float((self.windowHeight - y) * self.worldHeight / self.windowHeight)
 
-    # use WindowShouldClose() to decide how long to run
     def drawWindowFromArray(self, creatureArray, objectArray):
         BeginDrawing()
         ClearBackground(RAYWHITE)
@@ -57,6 +64,7 @@ class UI:
 
         EndDrawing()
 
+    # draws the world with all the creatures and walls. The creature also show their current velocity vector
     def drawWindow(self, creatures, walls=[], timePassed=0):
         BeginDrawing()
         ClearBackground(RAYWHITE)
@@ -64,6 +72,7 @@ class UI:
         for creature in creatures: # draws all the creatures
             if np.isnan(creature.location[0]) or np.isnan(creature.location[1]):
                 continue
+
             (posX, posY) = self.worldToWindow(creature.location[0], creature.location[1])
             DrawCircle(posX, posY, 5, BLACK)
 
@@ -108,20 +117,22 @@ def showSimulation(filepath):
     ui.closeWindow()
 
 
+# example scene that will be played when running ui.py without any additional parameters
 def testUI():
-    import lane, bottlekneck
+    import lane, bottlekneck, evacuate, crossing
 
-    creatures = lane.creatures()
+    creatures = lane.creatures()# or bottlekneck, evacuate, crossing
     walls = lane.objects()
 
     ui = UI(800, 800, 10, 10)
 
     timePassed = 0
 
-    while not WindowShouldClose() and timePassed < 20:
+    # update loop (simulates at most 60 seconds)
+    while not WindowShouldClose() and timePassed < 60:
         ui.drawWindow(creatures, walls, timePassed)
 
-        dt = 0.01 # GetFrameTime()
+        dt = 0.01
 
         for robot in creatures:
             robot.update(simple.socialForce, creatures, walls, dt)
