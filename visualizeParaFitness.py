423043d80d6984e7e490e6e7adf5ba6221b1c4be
JoeMystery <joelandenmatten@me.com>
2021-12-05 14:22:51 +0100

    Named the parameters

--- visualizeParaFitness.py
+++ visualizeParaFitness.py
@@ -9,14 +9,17 @@ if __name__ == "__main__":
         array = np.load(filepath, allow_pickle=True)
 
         if array.ndim == 3:
+            parameterNames = [r'interaction strength A $f_{\alpha\beta}$',r'interaction range B of $f_{\alpha\beta}$',r'relaxation time $\tau$',r'interaction strength A $f_{\alpha i}$',r'interaction range B of $f_{\alpha i}$']
             for i in range(0, array.shape[2]):
                 x_axis = np.linspace(0, array.shape[0], array.shape[0])
                 y_axis = np.zeros(array.shape[0])
                 for j in range(0, array.shape[0]):
                     y_axis[j] = array[j][0][i]
-                plt.plot(x_axis, y_axis)
+                plt.plot(x_axis, y_axis,label=parameterNames[i])
         else:
             x_axis = np.linspace(0, array.shape[0], array.shape[0])
             plt.plot(x_axis, array)
-
+        plt.legend(loc='best')
+        plt.xlabel('Generations')
+        plt.ylabel('Parameter Value')
         plt.show()
