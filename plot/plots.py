c10750090d8056c1d28469c96e1e8b2a80197345
Klark007 <jan.grunder@yahoo.de>
2021-12-05 15:13:16 +0100

    Comments in plot.py

--- plot/plots.py
+++ plot/plots.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import os, time
+import os
 
 from models import simple
 
+"""
+    This file is used to run a simulation with default parameters and then show those in plots containing
+        * positions
+        * velocites over time
+        * walls
+    
+    It can also show the different creatures each in their own file which is then saved into tmp
+"""
 
+# simulates and plots all the creatures, objects and velocities
 def plotCreatures(creatures, objects, numberOfTimesteps, dt):
     fig, ax = plt.subplots(ncols=2)
     locations, velocities = collectData(creatures, objects, numberOfTimesteps, dt)
@@ -15,6 +24,7 @@ def plotCreatures(creatures, objects, numberOfTimesteps, dt):
 
     plt.show()
 
+# plots a single creature and saves it into dir
 def plotCreature(idx, objects, locations, velocities, numberOfTimesteps, dt, dirName):
     fig, ax = plt.subplots(ncols=2)
 
@@ -29,6 +39,7 @@ def plotCreature(idx, objects, locations, velocities, numberOfTimesteps, dt, dir
     plt.savefig(filename)
 
 
+# does the simulation
 def collectData(creatures, objects, numberOfTimesteps, dt):
     numberOfCreatures = len(creatures)
     locations = np.zeros((numberOfTimesteps, numberOfCreatures, 2))
@@ -46,6 +57,7 @@ def collectData(creatures, objects, numberOfTimesteps, dt):
     return locations, velocities
 
 
+# plots all the objects (walls)
 def plotObjects(ax, objects):
     for object in objects:
         x = np.array([object.start[0], object.end[0]])
@@ -53,6 +65,7 @@ def plotObjects(ax, objects):
         ax[0].plot(x, y, color='black', lw=5)
 
 
+# plot location of a single creature (at index idx)
 def plotLocation(ax, idx, locations):
     ax[0].plot(locations[..., idx, 0], locations[..., idx, 1], label='Pedestrian #{}'.format(idx))
 
@@ -61,6 +74,7 @@ def plotLocation(ax, idx, locations):
     ax[0].set_ylabel(r'y-axis $[m]$')
 
 
+# plot all locations of all creatures
 def plotLocations(ax, creatures, locations):
     numberOfCreatures = len(creatures)
 
@@ -73,6 +87,7 @@ def plotLocations(ax, creatures, locations):
     return
 
 
+# plot velocity of a single creature (at index idx)
 def plotVelocity(ax, idx, velocities, numberOfTimesteps, dt):
     t = np.linspace(0, dt * numberOfTimesteps, numberOfTimesteps)
 
@@ -83,6 +98,7 @@ def plotVelocity(ax, idx, velocities, numberOfTimesteps, dt):
     ax[1].set_ylabel(r'velocity $[m/s]$')
 
 
+# plot all velocities of all creatures
 def plotVelocities(ax, creatures, velocities, numberOfTimesteps, dt):
     numberOfCreatures = len(creatures)
     t = np.linspace(0, dt * numberOfTimesteps, numberOfTimesteps)
